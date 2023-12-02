ee95a1fc4ef5ca59280d549932414554c6240eb8
Vadim <vadim23452@yandex.ru>
2023-12-02 16:41:17 +0300

    service for class car

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,31 @@
+package web.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+  private final List<Car> carList;
+
+   {
+    carList = new ArrayList<>();
+    carList.add(new Car("Volvo","sedan", 2014));
+    carList.add(new Car("Mercedes", "sedan", 2018));
+    carList.add(new Car("Toyota", "hatchback", 2011));
+    carList.add(new Car("Lada", "universal", 2012));
+    carList.add(new Car("Geep", "pick-up", 2022));
+  }
+
+  @Override
+  public List<Car> getCarsByCount(int count) {
+    if (count == 0) {
+      return carList;
+    } else {
+      return carList.stream().limit(count).collect(Collectors.toList());
+    }
+  }
+}
